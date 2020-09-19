e917be5e1530a892092b1378792ba5193fc178a8
Xiao <xiao.ye@alumnos.upm.es>
2020-09-19 21:01:09 +0200

    Añadido la funcionalidad de obtener el enlace de invitación de discord

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Crear y modificar el fichero src/bot_conf.json con el siguiente formato
 ```json
 {
     "token": "token del bot de telegram",
+    "permitted_group_id":"restringir que un único grupo de telegram pueda usar este bot",
     "discord_bot_host": "host del bot de discord donde desea enviar los mensajes del canal con su puerto, si estas en el mismo equipo prueba localhost:5000",
     "host_username": "usuario para acceder al host",
     "host_password": "contraseña del usuario"
--- src/bot.py
+++ src/bot.py
@@ -5,11 +5,32 @@ import base64
 
 CONF = json.load(open('src/bot_conf.json', 'r'))
 
-URL = 'http://' + CONF['discord_bot_host'] + '/sendChannelPost'
-tele_bot = tb.TeleBot(CONF['token'])
+URL = 'http://' + CONF['discord_bot_host']
+bot = tb.TeleBot(CONF['token'])
 
 
-@tele_bot.channel_post_handler(content_types=['text', 'photo'])
+text_messages = {
+    'welcome':
+        u'Welcome to ACM group{name}!',
+    'link_invitation':
+        u'@{name}, use this link {link} to join our discord server',
+    'only_private_command':
+        u'This command is only available in private, try to start a private conversation with me',
+    'not_member':
+        u'You must be a member of our telegram group'
+}
+
+text_server = {
+    'error':
+        u'Error en la conexión'
+}
+
+
+def is_permitted_group(chat_id):
+    return chat_id == int(CONF['permitted_group_id'])
+
+
+@bot.channel_post_handler(content_types=['text', 'photo'])
 def resend_text_to_discord(post):
     """create a request of http (post) to the discord bot server when the bot received a post of the channel
 
@@ -18,8 +39,8 @@ def resend_text_to_discord(post):
     """
     image_str = None
     if post.photo:
-        downloaded_file = tele_bot.download_file(
-            tele_bot.get_file(post.photo[-1].file_id).file_path)
+        downloaded_file = bot.download_file(
+            bot.get_file(post.photo[-1].file_id).file_path)
         image_byte = base64.b64encode(downloaded_file)
         image_str = image_byte.decode('ascii')
     aviso = {"text": post.text, "caption": post.caption, "photo": image_str,
@@ -27,11 +48,49 @@ def resend_text_to_discord(post):
                  "username": CONF["host_username"], "password": CONF["host_password"]
              }}
     try:
-        req = requests.post(URL, json=aviso)
+        req = requests.post(URL + '/sendChannelPost', json=aviso)
         if req.text != "OK":
             print(req.text)
     except:
-        print('Error en la conexion, post no enviado')
+        print(text_server['error'])
 
 
-tele_bot.polling(none_stop=False, interval=0, timeout=20)
+@bot.message_handler(func=lambda m: True, content_types=['new_chat_participant'])
+def on_user_joins(message):
+    if not is_permitted_group(message.chat.id):
+        return
+
+    name = message.new_chat_participant.first_name
+    if hasattr(message.new_chat_participant, 'last_name') and message.new_chat_participant.last_name is not None:
+        name += u" {}".format(message.new_chat_participant.last_name)
+
+    if hasattr(message.new_chat_participant, 'username') and message.new_chat_participant.username is not None:
+        name += u" (@{})".format(message.new_chat_participant.username)
+
+    bot.reply_to(message, text_messages['welcome'].format(name=name))
+
+
+@bot.message_handler(commands=["discord"])
+def get_discord_link(message):
+    if message.chat.type != 'private':
+        bot.reply_to(message, text_messages['only_private_command'])
+        return
+
+    if not bot.get_chat_member(int(CONF['permitted_group_id']), message.from_user.id):
+        bot.reply_to(message, text_messages['not_member'])
+        return
+
+    try:
+        req = requests.post(URL + '/server/link_invitation', json={"user": {
+            "username": CONF["host_username"], "password": CONF["host_password"]
+        }})
+        if req.text.startswith("https://"):
+            bot.reply_to(message, text_messages['link_invitation'].format(
+                name=message.from_user.first_name, link=req.text))
+        else:
+            print(req.text)
+    except:
+        print(text_server['error'])
+
+
+bot.polling(none_stop=False, interval=0, timeout=20)
